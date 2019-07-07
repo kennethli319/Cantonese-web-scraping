db68157c0df69ec1880a91a5155493a3fd0da676
Kenneth Li <wangyauli2@Kennethsyummymac.local>
2019-07-08 00:02:49 +0100

    basic

---
+++ search.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+try: 
+    from googlesearch import search 
+except ImportError:  
+    print("No module named 'google' found") 
+
+# to search 
+query = "啲哋嘢咩靚呢嗰唔佢你我係嘅咗咁架冇同咩哋幾啦畀俾啩啫嗱喇掂啫話睇喺咪喎仲揾哂囉啦呀諗吖埋咋蚊晝瞓嘥佬拎舊攰啱噚叻吓冧呃"
+  
+for each in query:
+    print(each)
+    for j in search(each, tld='com', num=300, start=0, stop=None, pause=5): 
+        print(j) 
+
+# https://core.ac.uk/download/pdf/48548269.pdf
+# YEUNG, Yu Ting 
+# Language Modeling for Speech Recognition of Spoken Cantonese 
+
+# 啲 哋 嘢 咩 靚 呢 嗰 唔 佢 你 我 係 嘅 咗 咁 
+# 架 冇 同 咩 哋 幾 啦 畀 俾 啩 啫 嗱 喇 掂 啫 
+# 話 睇 喺 咪 喎 仲 揾 哂 囉 啦 呀 諗 吖 埋 咋
+# 蚊 晝 瞓 嘥 佬 拎 舊 攰 啱 噚 叻 吓 冧 呃
+
+# 的 們 那 這 哪 在 不 是 了 些 他 她
